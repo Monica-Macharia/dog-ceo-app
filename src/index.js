67b497aaf6fc58feb27400fa166560713471d3f9
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-06 05:29:35 +0300

    feature: fetch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 console.log('%c HI', 'color: firebrick')
 
 function displayFun(item){
-
+    
 }
 
 
 fetch("https://dog.ceo/api/breeds/image/random/4")
 .then(response => response.json())
-.then(data => console.log(data) )
+.then(data => data.message.map(item => displayFun(item)))
 // data.map(item => displayFun(item))
